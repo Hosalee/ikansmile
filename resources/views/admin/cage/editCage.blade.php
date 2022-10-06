51e109a537b5c7b993964ec3fb231f4553c032b1
Hosalee <chalee099310@gmail.com>
2022-10-07 01:33:33 +0700

    Updatefontend

--- resources/views/admin/cage/editCage.blade.php
+++ resources/views/admin/cage/editCage.blade.php
@@ -7,7 +7,8 @@
 
 
 <main class="col bg-faded py-4 flex-grow-1">
-    <div class="container mt-5">
+    <div class="row mt-5"></div>
+    <div class="container mt-3">
         <div class="row">
             <div class="col-lg-12 text-center">
                 <h2>แก้ไขข้อมูลกระชัง</h2>
@@ -87,11 +88,11 @@
                     </div>
                
                    
-                    <div class="col-md-12 form-inline">
+                    <div class="col-md-12 form-inline ml-3">
+                        <div class="m-lg-2">
+                            <button type="submit" class="mt-3 btn btn-Warning">Update</button></div>
                         <div class="">
-                            <button type="submit" class="mt-3 btn btn-primary">Update</button></div>
-                        <div class="">
-                            <a href="{{ route('cage') }}" class="btn btn-danger ml-4 mt-3 ">ย้อนกลับ</a>
+                            <a href="{{ route('cage') }}" class="btn btn-danger ml-4 mt-3 px-4 ">Back</a>
                         </div>
                     </div>
                   
